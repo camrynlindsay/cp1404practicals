da0e0ac09afc1a1c1c3cb4dc14da078e4aff4494
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-09-23 17:15:43 +1000

    Add function docstrings to password_stars.py

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -2,12 +2,14 @@ PASSWORD_LENGTH_THRESHOLD = 8
 
 
 def main():
+    """This programs asks a user for a valid password and prints out asteriks based on
+    the length of the password"""
     password = get_password()
     print_asterisks(password)
-    print("Password set")
 
 
 def get_password():
+    """Get a valid password from user"""
     password = input("Enter password: ")
     while len(password) < PASSWORD_LENGTH_THRESHOLD:
         print("Password must be 8 characters or more long.")
@@ -16,6 +18,7 @@ def get_password():
 
 
 def print_asterisks(password):
+    """Print asteriks based on the length of the password input"""
     print(len(password) * "*")
 
 
