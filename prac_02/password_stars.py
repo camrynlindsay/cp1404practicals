6cbc59b86f5e22583039baeee5f498919cdfd41a
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-09-23 16:40:18 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,8 +1,22 @@
 PASSWORD_LENGTH_THRESHOLD = 8
 
-password = input("Enter password: ")
-while len(password) < PASSWORD_LENGTH_THRESHOLD:
-    print("Password must be 8 characters or more long.")
+
+def main():
+    password = get_password()
+    print_asterisks(password)
+    print("Password set")
+
+
+def get_password():
     password = input("Enter password: ")
-print(len(password) * "*")
-print("Password set")
+    while len(password) < PASSWORD_LENGTH_THRESHOLD:
+        print("Password must be 8 characters or more long.")
+        password = input("Enter password: ")
+    return password
+
+
+def print_asterisks(password):
+    print(len(password) * "*")
+
+
+main()
