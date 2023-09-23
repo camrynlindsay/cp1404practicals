e0ceb6fb2669cdc0755824b0b20335c2a8801f6c
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-09-23 18:10:02 +1000

    Add functions from scores.py and password_stars.py to program

--- prac_02/score_menu.py
+++ prac_02/score_menu.py
@@ -1,16 +1,45 @@
 MENU = "(G)et Score\n(P)rint Result\n(S)how Stars\n(Q)uit"
 
-print(MENU)
-choice = input(">>> ").upper()
-while choice != "Q":
-    if choice == "G":
-        pass
-    elif choice == "P":
-        pass
-    elif choice == "S":
-        pass
-    else:
-        print("Invalid option.")
+
+def main():
     print(MENU)
     choice = input(">>> ").upper()
-print("Farewell.")
+    while choice != "Q":
+        if choice == "G":
+            score = get_valid_score()
+        elif choice == "P":
+            result = determine_score(score)
+            print(result)
+        elif choice == "S":
+            print_stars(score)
+        else:
+            print("Invalid option.")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell.")
+
+
+def get_valid_score():
+    score = int(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
+
+
+def determine_score(score):
+    """Determine the result based on the score input"""
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+def print_stars(score):
+    """Print number of stars based on score"""
+    print(score * "*")
+
+
+main()
