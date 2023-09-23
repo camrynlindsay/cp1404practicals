085f400a4b2b23d5955c971b956b1daaa46432c5
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-09-23 17:29:35 +1000

    Add functions to score.py

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,13 +3,29 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
+import random
+
+
+def main():
+    """This program takes a score input and prints out the result"""
     score = float(input("Enter score: "))
-if score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    print(determine_score(score))
+    random_score = random.randint(0, 100)
+    print(random_score)
+    print(determine_score(random_score))
+
+
+def determine_score(score):
+    """Determine the result based on the score input"""
+    if score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
