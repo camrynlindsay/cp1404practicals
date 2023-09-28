f40be1af30caeb638aa0c09d5172eaf168290075
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-09-28 19:06:52 +1000

    Add code for question 3

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,14 +1,20 @@
 FILENAME = "name.txt"
 
 # Question 1
-name = input("Name: ")
-out_file = open(FILENAME, "w")
-print(name, file=out_file)
-out_file.close()
+# name = input("Name: ")
+# out_file = open(FILENAME, "w")
+# print(name, file=out_file)
+# out_file.close()
 
 # Question 2
-in_file = open(FILENAME, 'r')
-text = in_file.read()
-in_file.close()
-print(f"Your name is {text}")
+# in_file = open(FILENAME, 'r')
+# text = in_file.read()
+# in_file.close()
+# print(f"Your name is {text}")
 
+# Question 3
+input_file = open("numbers.txt", 'r')
+first_number = int(input_file.readline().strip())
+second_number = int(input_file.readline().strip())
+result = first_number + second_number
+print(result)
