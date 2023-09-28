17cc758e4f00041c28379ea924f693f578d7933c
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-09-28 18:23:04 +1000

    Fix placement of comments, changed code to answer question 3.

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,17 +2,22 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+A ValueError will occur when input other than integers (int) are entered.
 2. When will a ZeroDivisionError occur?
+A ZeroDivisionError will occur when 0 is entered in the denominator (Math Error - undefined).
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+The code can change to place an error-checker before the calculation to see if the denominator is equal to
+zero or not. Therefore, can delete the ZeroDivisionError exception because it will never be reached.
 """
 
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Cannot divide by 0, try again!")
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
-except ValueError:  # occurs when input other than integers (int) are entered.
+except ValueError:
     print("Numerator and denominator must be valid numbers!")
-except ZeroDivisionError:  # occurs when 0 is entered in the denominator (Math Error - undefined).
-    print("Cannot divide by zero!")
 print("Finished.")
