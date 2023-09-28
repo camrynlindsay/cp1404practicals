773567ad3e905046c31c5bc639e2e981f37adde0
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-09-28 18:49:21 +1000

    Add code for question 2, add comment headings for questions

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,4 +1,14 @@
+FILENAME = "name.txt"
+
+# Question 1
 name = input("Name: ")
-out_file = open("name.txt", "w")
+out_file = open(FILENAME, "w")
 print(name, file=out_file)
 out_file.close()
+
+# Question 2
+in_file = open(FILENAME, 'r')
+text = in_file.read()
+in_file.close()
+print(f"Your name is {text}")
+
