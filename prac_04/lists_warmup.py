aa95629ac9b82fb76470367baaca4c7f928dd1fe
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-10-04 12:50:08 +1000

    Complete questions 1-4 in 'lists_warmup.py'

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -3,11 +3,11 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # numbers[0] will produce 3
 # numbers[-1] will produce 2
 # numbers[3] will produce 1
-# numbers[:-1] will produce 3, 1, 4, 1, 5, 9
-# numbers[3:4] will produce 1
+# numbers[:-1] will produce [3, 1, 4, 1, 5, 9]
+# numbers[3:4] will produce [1]
 # 5 in numbers will produce True
 # 7 in numbers will produce False
-# "3" in numbers will produce a TypeError ("3" is a string)
+# "3" in numbers will produce False
 # numbers + [6, 5, 3] will produce [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
 """
@@ -16,3 +16,8 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 3. Print all the elements from numbers except the first two (slice)
 4. Print whether 9 is an element of numbers
 """
+
+numbers[0] = "ten"
+numbers[-1] = 1
+print(numbers[2:])
+print(9 in numbers)
