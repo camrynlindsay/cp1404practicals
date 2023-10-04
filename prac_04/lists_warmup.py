9d135ea127104b82e5deedd18561af379d346f62
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-10-04 12:37:19 +1000

    Add answers for step 1 in 'lists_warmup.py'

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,18 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0] will produce 3
+# numbers[-1] will produce 2
+# numbers[3] will produce 1
+# numbers[:-1] will produce 3, 1, 4, 1, 5, 9
+# numbers[3:4] will produce 1
+# 5 in numbers will produce True
+# 7 in numbers will produce False
+# "3" in numbers will produce a TypeError ("3" is a string)
+# numbers + [6, 5, 3] will produce [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+"""
+1. Change the first element of numbers to "ten" (the string, not the number 10)
+2. Change the last element of numbers to 1
+3. Print all the elements from numbers except the first two (slice)
+4. Print whether 9 is an element of numbers
+"""
