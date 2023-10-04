82e7b3699f26bff3e8cbc5f7a0fe9349389f687b
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-10-04 19:36:50 +1000

    Complete program for 'quick_picks.py'

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,7 +1,16 @@
 import random
 
+NUMBERS_IN_LINE = 6
 LOWEST_NUMBER = 1
 HIGHEST_NUMBER = 45
 
 number_of_quick_picks = int(input("How many quick picks? "))
 for i in range(number_of_quick_picks):
+    numbers = []
+    for j in range(NUMBERS_IN_LINE):
+        number = random.randint(LOWEST_NUMBER, HIGHEST_NUMBER)
+        while number in numbers:
+            number = random.randint(LOWEST_NUMBER, HIGHEST_NUMBER)
+        numbers.append(number)
+    numbers.sort()
+    print(" ".join(f"{number:2}" for number in numbers))
