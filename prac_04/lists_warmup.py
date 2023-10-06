c6214fc5e2f8ca4d9ea18f09269eff6254f18c3f
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-10-06 10:05:14 +1000

    Fix formatting for better readability

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -17,7 +17,11 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 4. Print whether 9 is an element of numbers
 """
 
+# 1
 numbers[0] = "ten"
+# 2
 numbers[-1] = 1
+# 3
 print(numbers[2:])
+# 4
 print(9 in numbers)
