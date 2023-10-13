2c18132455074023b545ba51c0ed469aef3a5ba0
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-10-13 12:58:52 +1000

    Add function docstrings to prac 5 'wimbledon.py'

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -8,12 +8,14 @@ FILENAME = "wimbledon.csv"
 
 
 def main():
+    """Read, process and display the information from the wimbledon file."""
     records = get_records(FILENAME)
     champion_to_count, countries = process_records(records)
-    display_results(champion_to_count, countries)
+    display_records(champion_to_count, countries)
 
 
 def get_records(filename):
+    """Get the records from the input file."""
     records = []
     with open(filename, "r", encoding="utf-8-sig") as in_file:
         in_file.readline()
@@ -24,6 +26,7 @@ def get_records(filename):
 
 
 def process_records(records):
+    """Process the records into a set and dictionary."""
     champion_to_count = {}
     countries = set()
     for record in records:
@@ -35,7 +38,8 @@ def process_records(records):
     return champion_to_count, countries
 
 
-def display_results(champion_to_count, countries):
+def display_records(champion_to_count, countries):
+    """Display the champions and countries."""
     print("Wimbledon Champions: ")
     for name, count in champion_to_count.items():
         print(name, count)
