f36272aa8f9743c80c8b21a802baee586544a4e5
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-10-25 21:12:17 +1000

    Complete 'guitar.py', 'guitar_test.py' and 'guitars.py' in prac 6

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,19 @@
+from guitar import Guitar
+
+
+def run_tests():
+    """Test if class Guitar methods work. """
+    name = "Gibson L-5 CES"
+    year = 1922
+    cost = 16035.40
+
+    guitar = Guitar(name, year, cost)
+    thing = Guitar("Another Guitar", 2011, 20000)
+
+    print(f"{guitar.name} get_age() - Expected {101}. Got {guitar.get_age()}")
+    print(f"{thing.name} get_age() - Expected {12}. Got {guitar.get_age()}")
+    print(f"{guitar.name} is_vintage() - Expected {True}. Got {guitar.is_vintage()}")
+    print(f"{guitar.name} is_vintage() - Expected {False}. Got {thing.is_vintage()}")
+
+
+run_tests()
