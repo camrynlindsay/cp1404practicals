e42bfcd237f0949db6f3046b8f2a7e75ab91a18d
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-10-27 11:17:27 +1000

    Fix indentation in 'guitars.py' in prac 6

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -19,16 +19,16 @@ def main():
         guitars.append(new_guitar)
         name = input("Name: ")
 
-        guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-        guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
-        if guitars:
-            print("These are my guitars:")
-            for i, guitar in enumerate(guitars, start=1):
-                vintage_string = ""
-                if guitar.is_vintage():
-                    vintage_string = "(vintage)"
-                print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth $ {guitar.cost:10,.2f} {vintage_string}")
+    if guitars:
+        print("These are my guitars:")
+        for i, guitar in enumerate(guitars, start=1):
+            vintage_string = ""
+            if guitar.is_vintage():
+                vintage_string = "(vintage)"
+            print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth $ {guitar.cost:10,.2f} {vintage_string}")
 
 
 main()
