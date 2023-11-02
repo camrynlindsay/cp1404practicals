c57e15ee8e8f66d6fdf48e8f7989f2c474a1d8b4
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-11-02 17:40:16 +1000

    Add menu structure in main, add function headings

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,7 +1,7 @@
 class Project:
     """Represent information about a project."""
 
-    def __init__(self, name="", start_date="", priority=0, cost_estimate=0, percentage=0):
+    def __init__(self, name="", start_date="", priority=0, cost_estimate=0.0, percentage=0):
         """Initialise values about a project."""
         self.name = name
         self.start_date = start_date
@@ -11,5 +11,5 @@ class Project:
 
     def __repr__(self):
         """Return the string representation of the class Project."""
-        return f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate}," \
-               f" completion: {self.percentage}%"
+        return f"{self.name}, start: {self.start_date}, priority {self.priority}, " \
+               f"estimate: ${self.cost_estimate:.2f}, completion: {self.percentage}%"
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -1,5 +1,65 @@
 """
 Estimate: 90 minutes
-Actual: __ minutes
+Actual: __ minutes  6
 """
 
+import datetime
+from project import Project
+
+# date_string = input("Date (d/m/yyyy): ")  # e.g., "30/9/2022"
+# date = datetime.datetime.strptime(date_string, "%d/%m/%Y").date()
+# # print(f"That day is/was {date.strftime('%A')}")
+# print(date.strftime("%d/%m/%Y"))
+
+MENU = "(L)oad projects\n(S)ave projects\n(D)isplay projects\n(F)ilter projects by date\n(A)dd new project\n" \
+       "(U)pdate project\n(Q)uit"
+
+
+def main():
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "L":
+            load_projects()
+        elif choice == "S":
+            save_projects()
+        elif choice == "D":
+            display_projects()
+        elif choice == "F":
+            filter_projects()
+        elif choice == "A":
+            add_new_project()
+        elif choice == "U":
+            update_project()
+        else:
+            print("Invalid input.")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Farewell.")
+
+
+def load_projects():
+    pass
+
+
+def save_projects():
+    pass
+
+
+def display_projects():
+    pass
+
+
+def filter_projects():
+    pass
+
+
+def add_new_project():
+    pass
+
+
+def update_project():
+    pass
+
+
+main()
