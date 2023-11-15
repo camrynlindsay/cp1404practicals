bda5428a6b8d68c364e961a299ae874b0286729a
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-11-15 18:14:28 +1000

    Create and initialise UnreliableCar class

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,19 @@
+import random
+from prac_09.car import Car
+
+
+class UnreliableCar(Car):
+    """Specialised version of a Car that includes reliability."""
+
+    def __init__(self, name, fuel, reliability):
+        """Initialise an UnreliableCar instance, based on parent class Car."""
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        """Drive a specified distance only if the car is reliable."""
+        random_number = random.randint(0, 100)
+        if random_number > self.reliability:
+            distance = 0
+        distance_driven = super().drive(distance)
+        return distance_driven
