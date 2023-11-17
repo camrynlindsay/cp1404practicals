824b4e0e2358a649ff60d8fb26046980425f47a8
camrynlindsay <camryn.lindsay@my.jcu.edu.au>
2023-11-17 10:46:04 +1000

    Complete play method in Band class to match expected output in my_band.py - prac 09

--- prac_09/band.py
+++ prac_09/band.py
@@ -11,22 +11,20 @@ class Band:
     def __init__(self, name):
         """Construct values for a Band instance."""
         self.name = name
-        self.members = []
+        self.instruments = []
+        self.musicians = []
 
     def __str__(self):
         """Return a string representation of a Band."""
-        return f"{self.name} ({', '.join(str(musician) for musician in self.members)})"
+        return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
 
-    def add(self, member):
-        """Add a member to the band."""
-        self.members.append(member)
+    def add(self, musician):
+        """Add a musician to the band."""
+        self.musicians.append(musician)
 
     def play(self):
-        """"""
-        pass
-        # for member in self.members:
-        #     if member
-        #
-        # return f"{self.name} is playing: {self.instruments[0]}"
-        # if self.instruments:
-        # return f"{self.name} is playing: {self.instruments[0]}"
+        """Return a string showing the musician/s playing their first instrument."""
+        results = []
+        for musician in self.musicians:
+            results.append(musician.play())
+        return "\n".join(results)
